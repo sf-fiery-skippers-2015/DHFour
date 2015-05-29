164f4f5cceb018d73a8619a470ff3b9789f48f9e
Mary Wallace <wallaceemee@gmail.com>
2015-05-28 17:17:06 -0700

    psuedocode and some working solution for row

--- dhfour.js
+++ dhfour.js
@@ -18,3 +18,56 @@ var player2 = player_images.pop
 
 // win:
 // change display message to say who won and stop play cycle.
+
+
+
+// Looking for four instances of the image in a row based on index
+// If yes, check source to see if they are the same
+// JQUERY $.unique() to look and compare the urls
+// If length is one then there is a win 
+
+
+
+var rowOne = [];
+var counts = {};
+
+jQuery.each(rowOne, function(key, value) {
+  if (!counts.hasOwnProperty(value)) {
+    counts[value] = 1;
+  } else {
+    counts[value]++;
+  }
+});
+
+for(var key in counts) {
+  rowOne.push(counts[key]);
+};
+
+
+
+// for (var num = 1; num < 7; num++) {
+//  rowArray = $(".row:nth-child("+ num +") img[src= name]")
+
+//    // if there are four of any picture in a row
+
+//    if (rowArray.length === 4) {
+//      classNumbers = []
+
+//      // gather the class numbers of the pictures
+
+//      rowArray.parent().each(function() {
+//       classNumbers.push($(this).attr("class").split(' ')[1])
+//     })
+
+//    } // end if loop
+
+//  // check to see if there are four instances of any pictures
+
+//    // if so, gather the column numbers
+
+//    // check if the numbers ascend
+
+// } // end for loop
+
+
+
